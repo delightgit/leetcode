c4a3623161a2c9c7ce670bddbf42c01d4c1c787e
Delight Lee <98308370+delightgit@users.noreply.github.com>
2023-08-06 17:21:05 -0700

    Create 2798. Number of Employees Who Met the Target.java

---
+++ 2798. Number of Employees Who Met the Target.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
+        return (int) Arrays.stream(hours)
+            .filter(hour -> hour >= target)
+            .count();
+    }
+}
