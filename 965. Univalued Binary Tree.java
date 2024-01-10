f4152c3abbaca92d31ec87d8055d26b0207d82c6
Delight Lee <98308370+delightgit@users.noreply.github.com>
2024-01-09 20:36:30 -0800

    Create 965. Univalued Binary Tree.java

---
+++ 965. Univalued Binary Tree.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isUnivalTree(TreeNode root) {
+        int value = root.val;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()){
+            TreeNode curr = q.poll(); 
+            if(curr.val != value){
+                return false;
+            }
+            if(curr.left != null){
+                q.add(curr.left);
+            }
+            if(curr.right != null){
+                q.add(curr.right);
+            }
+        }
+        return true;
+    }
+}
