7bdff1105962bda375993ed314043dfe7170e343
Delight Lee <98308370+delightgit@users.noreply.github.com>
2024-01-22 20:55:05 -0800

    Create 771. Jewels and Stones.java

---
+++ 771. Jewels and Stones.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        Set<Character> set = new HashSet<>();
+        for(int i = 0; i < jewels.length(); i++){
+            set.add(jewels.charAt(i));
+        }
+        int total = 0;
+        for(int i = 0; i < stones.length(); i++){
+            if(set.contains(stones.charAt(i))){
+                total++;
+            }
+        }
+        return total;
+    }
+}
